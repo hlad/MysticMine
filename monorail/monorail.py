15402dde94a08a49f96abcb99d409c0245281b10
hlad <hladma@gmail.com>
2024-07-10 15:24:56 +0200

    remove imp module for python 3.12 compatibility

--- monorail/monorail.py
+++ monorail/monorail.py
@@ -6,13 +6,13 @@ import copy
 import time
 import traceback
 import inspect
-import imp
 
 #http://stackoverflow.com/questions/606561/how-to-get-filename-of-the-main-module-in-python
 def main_is_frozen():
    return (hasattr(sys, "frozen") or # new py2exe
            hasattr(sys, "importers") # old py2exe
-           or imp.is_frozen("__main__")) # tools/freeze
+           or False)
+#           or imp.is_frozen("__main__")) # tools/freeze
 
 def get_main_dir():
    if main_is_frozen():
